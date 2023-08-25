d034b38bfa209aaf2211160152ba22aea1592532
SKY-PEY <lovr8537@gmail.com>
2023-08-25 12:16:05 +0900

    Creat project

---
+++ src/store/about.js
@@ -0,0 +1,9 @@
+import {Store} from '../core/heropy'
+
+export default new Store ({
+  photo : 'https://heropy.blog/css/images/logo.png',
+  name: 'PEY / ParkEunYeong',
+  email: 'lovr8537@gmail.com',
+  github: 'https://github.com/SKY-PEY',
+  repository: "https://github.com/SKY-PEY/vanilajs-movie-app-0824"
+})
